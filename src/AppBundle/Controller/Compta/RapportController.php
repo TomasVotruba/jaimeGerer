5db5481c8fcbc0303a7df6b8e8ec0b466219e109
logilquin <lo.gilquin@yahoo.fr>
2018-01-25 09:31:40 +0100

    export balance

--- src/AppBundle/Controller/Compta/RapportController.php
+++ src/AppBundle/Controller/Compta/RapportController.php
@@ -337,7 +337,9 @@ class RapportController extends Controller
 				'choices' => array(
 					'ANNEE' => 'Année',
 					'TRIMESTRE' => 'Trimestre',
-					'MOIS' => 'Mois'
+					'MOIS' => 'Mois',
+					'ANNEE_PRECEDENTE' => 'Année précédente',
+					'ANNEE_COURS_ET_PRECEDENTE' => 'Année précédente et année en cours'
 				),
 				'label' => 'Période',
 				'data' => 'ANNEE',
