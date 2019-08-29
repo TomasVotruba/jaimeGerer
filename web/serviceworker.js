777a5bfda1d3d5fbb3eec5d7c54e9f6d94f121cb
logilquin <lo.gilquin@yahoo.fr>
2019-08-29 12:48:34 +0200

    manifest and icon

--- web/serviceworker.js
+++ web/serviceworker.js
@@ -1,12 +1,14 @@
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('sw-cache').then(function(cache) {
+
       return cache.add('css/admin.css');
     })
   );
 });
  
 self.addEventListener('fetch', function(event) {
+  caches.delete('sw-cache');
   event.respondWith(
     caches.match(event.request).then(function(response) {
       return response || fetch(event.request);
